f2a457a0aa4ef9dc81145ce021dfa38a21a8f101
Ahobilesan <gahobilesan@gmail.com>
2022-07-15 12:51:18 +0530

    Store updated

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,33 @@
-import { configureStore } from '@reduxjs/toolkit'
-import rootReducer from './reducer'
-// ...
+import { combineReducers, configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
+import createSagaMiddleware from 'redux-saga';
+import reducer from './reducer'
+import postReducer from "../slices/slice"
+import rootSaga from './saga';
 
-export const store = configureStore({ reducer: rootReducer as any })
+// ...
+const RootReducer = combineReducers({
+    root: reducer,
+    post: postReducer
+})
+
+function StoreSetup() {
+    const sagaMiddleware = createSagaMiddleware();
+    const middlewares = getDefaultMiddleware({
+        immutableCheck: false,
+        serializableCheck: false,
+    });
+    middlewares.push(sagaMiddleware);
+
+    const store = configureStore({
+        reducer: RootReducer,
+        middleware: middlewares,
+    })
+
+    sagaMiddleware.run(rootSaga);
+    return store
+}
+
+export const store = StoreSetup()
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
